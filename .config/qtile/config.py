183240c005a1f6605eeb326f024b0c6d30826037
alsersan <alsersan20@gmail.com>
2021-03-21 18:09:30 +0100

    change groups config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget
@@ -113,33 +87,19 @@ groups = [Group(i) for i in ["WWW", "DEV", "TERM", "MISC"]]
 for i, group in enumerate(groups):
     current_key = str(i + 1)
     keys.extend([
-        # Switch to workspace N
+        # Switch to selected group
         Key([mod], current_key, lazy.group[group.name].toscreen()),
-        # Send window to workspace N
+
+        # Send window to selected group
         Key([mod, "shift"], current_key, lazy.window.togroup(group.name))
+
+        # Or, send window to selected group and switch to that group
+        # Key([mod, "shift"], current_key, lazy.window.togroup(group.name, switch_group=True))
     ])
-
-#groups = [Group(i) for i in "123456789"]
-
-#for i in groups:
-#    keys.extend([
-#        # mod1 + letter of group = switch to group
-#        Key([mod], i.name, lazy.group[i.name].toscreen(),
- #           desc="Switch to group {}".format(i.name)),
-#
-#        # mod1 + shift + letter of group = switch to & move focused window to group
-#        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-#            desc="Switch to & move focused window to group {}".format(i.name)),
-#        # Or, use below if you prefer not to switch to that group.
-#        # # mod1 + shift + letter of group = move focused window to group
- #       # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-#        #     desc="move focused window to group {}".format(i.name)),
-#    ])
 
 layouts = [
     layout.Columns(border_focus_stack='#d75f5f'),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
